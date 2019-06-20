62f1f6d87611c17bfc645cc87ec19c17effe87ac
albertobezzon <albertobezzon@gmail.com>
2019-06-20 15:19:51 +0200

    Add prepare_dataset.py

---
+++ code/prepare_dataset.py
@@ -0,0 +1,41 @@
+import csv
+import os
+import shutil
+
+
+def read_csv():
+    metadata = {}
+
+    with open('HAM10000_metadata.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter = ',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                metadata.update({row[1]: row[2]})
+    return metadata
+
+# Problema se le directory esistono gia'
+def preparing_directory(metadata):
+    values = list(set([x for x in metadata.values()]))
+    for name in values:
+        if not os.path.exists(name):
+            os.mkdir(name)
+
+
+def subdivide_image(metadata):
+    dir_name = ['HAM10000_images_part_1', 'HAM10000_images_part_1']
+    for directory in dir_name:
+        for file_ in os.listdir(directory):
+            filename = os.path.splitext(file_)[0]
+            class_ = metadata[filename]
+            print(class_)
+            shutil.copyfile(directory+"/"+file_, class_+"/"+file_)
+            exit()
+
+
+if __name__ == "__main__":
+    metadata = read_csv()
+    preparing_directory(metadata)
+    subdivide_image(metadata)
